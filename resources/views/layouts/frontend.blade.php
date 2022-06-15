e335ccf7bf53ca1aca731f0c287293f67e59265a
Jinu Varghese <jinuvarghese201820@gmail.com>
2022-06-15 18:06:52 +0530

    commit

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -25,7 +25,6 @@
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                 @csrf
             </form>
-            @include('layouts.navbars.sidebar')
         @endauth
 
         <div class="main-content">
